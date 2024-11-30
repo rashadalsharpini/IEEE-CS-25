472c169263b3335bbced01ced21d81da9f46b3b1
rashad <rashadalsharpini@gmail.com>
2024-11-30 13:45:43 +0200

    task1

---
+++ Rookies/Task1/E - Keyboard.cpp
@@ -0,0 +1,103 @@
+// ﷽
+// Contest: Get Ready
+// Judge: Virtual Judge
+// URL: https://vjudge.net/contest/676467#problem/E
+// Memory Limit: 256
+// Time Limit: 2000
+// Start: 30 نوف, 2024 01:38:35 م
+//
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef Rashad
+#include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+#define fastio                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+cin.tie(NULL);
+
+#define int long long
+#define double long double
+#define all(a) (a).begin(), (a).end()
+#define allr(a) (a).rbegin(), (a).rend()
+#define sz(a) (int)(a).size()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define vi vector<int>
+#define pi pair<int,int>
+#define OO 2e9
+#define endl "\n"
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1};
+const int dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+
+template <typename T> istream &operator>>(istream &input, vector<T> &data) {
+  for (T &x : data)
+    input >> x;
+  return input;
+}
+template <typename T> ostream &operator<<(ostream &output, const vector<T> &data) {
+  for (const T &x : data)
+    output << x << " ";
+  return output;
+}
+const int MOD = 1e9+7;
+int fast_power(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res *= a % MOD;
+    a *= a % MOD;
+    b >>= 1;
+  }
+  return res;
+}
+vi primeFactors(int n) {
+  vi factors;
+  while (n % 2 == 0) {
+    factors.push_back(2);
+    n /= 2;
+  }
+  for (int i = 3; i <= sqrt(n); i += 2) {
+    while (n % i == 0) {
+      factors.push_back(i);
+      n /= i;
+    }
+  }
+  if (n > 2) {
+    factors.push_back(n);
+  }
+  return factors;
+}
+// 48 - 57 -> 0 - 9  65 - 90 -> A-Z 97 - 122 -> a-z
+
+void solve() {
+  char lr;cin >> lr;
+  string s;cin >> s;
+  string dumb = "qwertyuiopasdfghjkl;zxcvbnm,./";
+  if (lr == 'R' || lr == 'r') {
+    for (int i = 0; i < s.length(); i++) {
+      int pos = dumb.find(s[i]);
+      cout << dumb[pos - 1];
+    }
+  } else if (lr == 'L' || lr == 'l') {
+    for (int i = 0; i < s.length(); i++) {
+      int pos = dumb.find(s[i]);
+      cout << dumb[pos + 1];
+    }
+  }}
+int32_t main() {
+  /*freopen("whereami.in", "r", stdin);*/
+  /*freopen("whereami.out", "w", stdout);*/
+  fastio
+    int t = 1;
+  /*cin>>t;*/
+  while (t--)
+    solve();
+  return 0;
+}
